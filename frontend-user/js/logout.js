2cb26dba51a16679ed4af7de459937416a4b33ca
MohammadMancy <mohammad.mancy994@gmail.com>
2022-06-12 00:01:58 +0300

    Appling log out link

---
+++ frontend-user/js/logout.js
@@ -0,0 +1,18 @@
+document.getElementById("log-out").addEventListener("click",  ()=> {
+    // var token = localStorage.getItem("access_token")
+    const token = localStorage.getItem('access_token');
+    axios({
+  
+      method: "post",
+      url: "http://127.0.0.1:8000/api/v1/logout",
+      headers: { Authorization: `Bearer `+token}
+    }).then(function (response){
+            alert('User successfully logged out.')
+            localStorage.removeItem("access_token")
+            localStorage.removeItem("username")
+            window.location.href = "./../index.html"
+
+        })
+    });
+
+  
